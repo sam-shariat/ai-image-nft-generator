7981524216230f4b19549a1355a513161ea7a6f1
Sam Shariat <samshariat7@gmail.com>
2023-02-28 02:28:59 +0330

    adding contract

---
+++ contracts/AICollection.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.8.1/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.8.1/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.8.1/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@4.8.1/access/Ownable.sol";
+import "@openzeppelin/contracts@4.8.1/utils/Counters.sol";
+
+/// @custom:security-contact samshariat7@gmail.com
+contract AICollection is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("AICollection", "AIC") {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
